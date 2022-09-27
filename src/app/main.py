2d43c127ce98395f5c158d4226474b629916897b
skotwind-res <skotwind.res.2@gmail.com>
2022-09-27 21:40:45 +0300

    Added addition

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,6 @@
 """Simple flask app."""
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,6 +10,13 @@ def index():
     """Simple flask method."""
     return "Hello"
 
+
+@app.route('/add')
+def add():
+    return str(
+        request.args.get("a") +
+        request.args.get("b")
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
