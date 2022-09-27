2b4f9cb590d41c5ac1a11dd752aa67cae7347679
skotwind-res <skotwind.res.2@gmail.com>
2022-09-27 20:55:04 +0300

    Added linting fixes.

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,4 @@
+"""Simple flask app."""
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """Simple flask method."""
     return "Hello"
 
 
